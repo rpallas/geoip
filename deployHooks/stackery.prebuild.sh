dccc7f73d32ce4bb8042fe5fbf7b4b73f7ee4f86
Robbie Pallas <robbie.pallas@piksel.com>
2020-04-12 17:55:12 +0100

    fix pre-build script

--- deployHooks/stackery.prebuild.sh
+++ deployHooks/stackery.prebuild.sh
@@ -5,11 +5,23 @@ export MAXMIND_KEY=5fJVNzndSwOFXD6V
 echo "Getting latest Maxmind IP data"
 curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" | tar -C . -xzv --wildcards '*/*.mmdb'
 
-echo "Compress files in layer to zip file"
-tar -zcvf geoip-data.zip --wildcards */*.mmdb
+echo "Move geoip data to lambda layer"
+mv */*.mmdb src/geoIPDataLambdaLayer/nodejs/ipdata
 
-echo "Move zip to lambda layer"
-mv geoip-data.zip src/geoIPDataLambdaLayer/nodejs/ipdata/geoip-data.zip
+echo
+echo pwd `pwd`
+echo pwd .. `pwd ..`
+echo
+
+echo "Compress files in layer to zip file"
+tar -zcvf src/geoIPDataLambdaLayer.zip src/geoIPDataLambdaLayer
+
+echo
+echo ls: `ls -a`
+echo ls src: `ls -a src`
+echo ls .stackery: `ls -a .stackery`
+echo ls .stackery/src: `ls -a .stackery/src`
+echo
 
 echo "Cleaning up unneeded files"
 rm -r GeoLite2-*_*/
