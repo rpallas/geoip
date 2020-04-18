cb5d950f29ca900bde8e63f471dd342341b88014
Robbie Pallas <robbie.pallas@piksel.com>
2020-04-18 19:03:53 +0100

    debug logging

--- src/lookup/index.js
+++ src/lookup/index.js
@@ -1,8 +1,13 @@
 const Maxmind = require('maxmind')
 
+const dirTree = require("directory-tree");
+
 let ipDB;
 
 exports.handler = async event => {
+  const tree = dirTree(".");
+  console.log(`current directory tree: ${JSON.stringify(tree)}`)
+  
   if (!ipDB) {
     ipDB = await Maxmind.open('/opt/ipdata/GeoLite2-City.mmdb')
   }
