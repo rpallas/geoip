5830dc79be5a0bb537fe1b1e306b9b3fc63c466e
Robbie Pallas <robbie.pallas@piksel.com>
2020-04-18 15:02:30 +0100

    get ipdata layer content from s3

--- deployHooks/stackery.prebuild.sh
+++ deployHooks/stackery.prebuild.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-export MAXMIND_KEY=5fJVNzndSwOFXD6V
+# export MAXMIND_KEY=5fJVNzndSwOFXD6V
 
-echo "Getting latest Maxmind IP data"
-curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" | tar -C . -xzv --wildcards '*/*.mmdb'
+# echo "Getting latest Maxmind IP data"
+# curl "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$MAXMIND_KEY&suffix=tar.gz" | tar -C . -xzv --wildcards '*/*.mmdb'
 
-echo "Move geoip data to lambda layer"
-mv */*.mmdb src/geoIPDataLambdaLayer/nodejs/ipdata
+# echo "Move geoip data to lambda layer"
+# mv */*.mmdb src/geoIPDataLambdaLayer/nodejs/ipdata
 
-echo
-echo pwd `pwd`
-echo pwd .. `pwd ..`
-echo
+# echo
+# echo pwd `pwd`
+# echo pwd .. `pwd ..`
+# echo
 
-echo "Compress files in layer to zip file"
-tar -zcvf src/geoIPDataLambdaLayer.zip src/geoIPDataLambdaLayer
+# echo "Compress files in layer to zip file"
+# tar -zcvf src/geoIPDataLambdaLayer.zip src/geoIPDataLambdaLayer
 
-echo
-echo ls: `ls -a`
-echo ls src: `ls -a src`
-echo ls .stackery: `ls -a .stackery`
-echo ls .stackery/src: `ls -a .stackery/src`
-echo
+# echo
+# echo ls: `ls -a`
+# echo ls src: `ls -a src`
+# echo ls .stackery: `ls -a .stackery`
+# echo ls .stackery/src: `ls -a .stackery/src`
+# echo
 
-echo "Cleaning up unneeded files"
-rm -r GeoLite2-*_*/
+# echo "Cleaning up unneeded files"
+# rm -r GeoLite2-*_*/
